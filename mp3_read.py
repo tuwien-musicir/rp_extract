694a22ddf16136eea5c083e7f0c7243dd7655e1e
Tom <Tom@client214-128.wireless.umu.se>
2015-05-31 13:09:00 +0200

    added lame MP3 decoder and generic command line decoder check

--- mp3_read.py
+++ mp3_read.py
@@ -4,33 +4,62 @@
 # as there is no Python library for it, we need to use external tools (mpg123, lame, ffmpeg)
 
 import os # for calling external program for mp3 decoding
+import subprocess # for subprocess calls
 import tempfile
 from scipy.io import wavfile
 
+
+# check if a command exists on the system (without knowing the path, i.e. like Linux 'which')
+def cmd_exists(cmd):
+    return subprocess.call("type " + cmd, shell=True,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
 
 def mp3_read(filename):
 
     temp = tempfile.NamedTemporaryFile(suffix='.wav')
     # print 'gettempdir():', tempfile.gettempdir()
 
-    cmd = 'mpg123'
-    args = '-q -w "' + temp.name + '" "' + filename + '"'
-    print 'Decoding mp3: ', cmd, args
+    # check a number of MP3 Decoder tools if they are available
+    cmd=[]
+    args=[]
 
-    try:
-        # execute external command:
-        # import subprocess
-        #return_code = call('mpg123') # does work
-        #return_code = call(['mpg123',args]) # did not work
-        #print return_code
+    cmd.append('mpg123')
+    args.append ('-q -w "' + temp.name + '" "' + filename + '"')
 
-        os.popen(cmd + ' ' + args)
-        fs, data = wavfile.read(temp.name)
-        #os.remove(tempfile) # now done by finally part after temp.close() automtacally be tempfile class
+    cmd.append('lame')
+    args.append ('--quiet --decode "' + filename + '" "' + temp.name + '"')
 
-    finally:
-        # Automatically cleans up (deletes) the temp file
-        temp.close()
-        # print 'Exists after close:', os.path.exists(temp.name)
+    # TODO ADD ffmpeg
+
+    success = False
+
+    for i in range(len(cmd)):
+
+        if cmd_exists(cmd[i]):
+
+            print 'Decoding mp3 with: ', cmd[i], args[i]
+
+            try:
+                # execute external command:
+                # import subprocess
+                #return_code = call('mpg123') # does work
+                #return_code = call(['mpg123',args]) # did not work
+                #print return_code
+
+                os.popen(cmd[i] + ' ' + args[i])
+                fs, data = wavfile.read(temp.name)
+                #os.remove(tempfile) # now done by finally part after temp.close() automtacally be tempfile class
+                success = True
+
+            finally:
+                # Automatically cleans up (deletes) the temp file
+                temp.close()
+                # print 'Exists after close:', os.path.exists(temp.name)
+        if success:
+            break
+
+    # TODO:
+    # if not success:
+    # print error message: no decoder found
 
     return (fs, data)
